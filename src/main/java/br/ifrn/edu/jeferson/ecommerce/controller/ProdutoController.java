36a180aee168b7f3ba284a67ac3c8432bed416a1
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-01-04 16:48:39 -0300

    feat: adiciona rota de busca de um produto por seu id.

--- src/main/java/br/ifrn/edu/jeferson/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/ifrn/edu/jeferson/ecommerce/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package br.ifrn.edu.jeferson.ecommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ifrn.edu.jeferson.ecommerce.domain.dtos.ProdutoRequestDTO;
 import br.ifrn.edu.jeferson.ecommerce.domain.dtos.ProdutoResponseDTO;
 import br.ifrn.edu.jeferson.ecommerce.service.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
+@Tag(name = "Produtos", description = "API para gerenciamento de produtos")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    @Operation(summary = "Cria um produto")
     @PostMapping
     public ResponseEntity<ProdutoResponseDTO> salvar(@RequestBody @Valid ProdutoRequestDTO produtoRequestDTO) {
         return ResponseEntity.ok(produtoService.salvar(produtoRequestDTO));
+    }
+
+    @Operation(summary = "Busca um produtor por seu id")
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> buscar(@PathVariable Long id) {
+        return ResponseEntity.ok(produtoService.buscar(id));
     }
 }
--- src/main/java/br/ifrn/edu/jeferson/ecommerce/service/ProdutoService.java
+++ src/main/java/br/ifrn/edu/jeferson/ecommerce/service/ProdutoService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import br.ifrn.edu.jeferson.ecommerce.domain.Produto;
 import br.ifrn.edu.jeferson.ecommerce.domain.dtos.ProdutoRequestDTO;
 import br.ifrn.edu.jeferson.ecommerce.domain.dtos.ProdutoResponseDTO;
+import br.ifrn.edu.jeferson.ecommerce.exception.ResourceNotFoundException;
 import br.ifrn.edu.jeferson.ecommerce.mapper.ProdutoMapper;
 import br.ifrn.edu.jeferson.ecommerce.repository.ProdutoRepository;
 
@@ -17,9 +18,18 @@ public class ProdutoService {
     @Autowired
     private ProdutoMapper produtoMapper;
 
+    Produto buscarProduto(Long id) {
+        return produtoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Este produto não foi encontrado"));
+    }
+
     public ProdutoResponseDTO salvar(ProdutoRequestDTO produtoRequestDTO) {
         Produto produto = produtoMapper.toEntity(produtoRequestDTO);
         produtoRepository.save(produto);
+        return produtoMapper.toResponseDTO(produto);
+    }
+
+    public ProdutoResponseDTO buscar(Long id) {
+        Produto produto = buscarProduto(id);
         return produtoMapper.toResponseDTO(produto);
     }
 }
