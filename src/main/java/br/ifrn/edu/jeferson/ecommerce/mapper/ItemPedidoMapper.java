5453f5b487198329effd1ab6861fa8200e23b125
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-01-10 17:41:36 -0300

    refactor: adiciona BeanMapping no Mapper de ItemPedido.

--- src/main/java/br/ifrn/edu/jeferson/ecommerce/mapper/ItemPedidoMapper.java
+++ src/main/java/br/ifrn/edu/jeferson/ecommerce/mapper/ItemPedidoMapper.java
@@ -2,9 +2,11 @@ package br.ifrn.edu.jeferson.ecommerce.mapper;
 
 import java.util.List;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import br.ifrn.edu.jeferson.ecommerce.domain.ItemPedido;
 import br.ifrn.edu.jeferson.ecommerce.domain.dtos.ItemPedidoRequestDTO;
@@ -21,6 +23,7 @@ public interface ItemPedidoMapper {
     ItemPedidoResponseDTO toResponseDTO(ItemPedido itemPedido);
     List<ItemPedidoResponseDTO> toDTOList(List<ItemPedido> itens);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(source = "produtoId", target = "produto.id")
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "pedido", ignore = true)
